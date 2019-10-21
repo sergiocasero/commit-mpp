9e2d9133f0cdddb5e8c945431105ca68554745e9
Sergio Casero <sergio.casero@worldline.com>
2019-10-21 18:49:15 +0200

    Added Either

---
+++ app/src/commonMain/kotlin/com/sergiocasero/commit/result/Either.kt
@@ -0,0 +1,34 @@
+package com.sergiocasero.commit.result
+
+sealed class Either<L, R> {
+    class Left<L, R>(val error: L) : Either<L, R>() {
+        override fun toString(): String = "Left $error"
+    }
+
+    class Right<L, R>(val success: R) : Either<L, R>() {
+        override fun toString(): String = "Right $success"
+    }
+
+    infix fun <Rp> bind(f: (R) -> (Either<L, Rp>)): Either<L, Rp> {
+        return when (this) {
+            is Left<L, R> -> Left(this.error)
+            is Right<L, R> -> f(this.success)
+        }
+    }
+
+    infix fun <Rp> map(f: (R) -> (Either<L, Rp>)): Either<L, Rp> {
+        return when (this) {
+            is Left<L, R> -> Left(this.error)
+            is Right<L, R> -> f(this.success)
+        }
+    }
+
+    infix fun <Rp> seq(e: Either<L, Rp>): Either<L, Rp> = e
+
+    fun fold(error: (L) -> Unit, success: (R) -> Unit) {
+        when(this) {
+            is Left -> error(this.error)
+            is Right -> success(this.success)
+        }
+    }
+}
