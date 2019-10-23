eccc568c922a9d9b031ff99b696bd95ee13a4bc6
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2019-10-23 17:20:17 +0200

    iOS Working :)

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -4,7 +4,7 @@ import app
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = Proxy().proxyHello()
+        label.text = "Hello"
     }
 
     override func didReceiveMemoryWarning() {
