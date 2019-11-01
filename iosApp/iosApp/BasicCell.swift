072cf15e719f7b73fd70ae2dfd474a46779d4e81
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2019-11-01 09:13:55 +0100

    Added tablayout

---
+++ iosApp/iosApp/BasicCell.swift
@@ -0,0 +1,62 @@
+//
+//  BasicCell.swift
+//  MyPagerCollView
+//
+//  Created by Leela Prasad on 08/03/18.
+//  Copyright © 2018 Leela Prasad. All rights reserved.
+//
+
+import UIKit
+
+class BasicCell: UICollectionViewCell {
+    
+    let titleLabel: UILabel = {
+        let lbl = UILabel()
+        return lbl
+    }()
+    
+    var indicatorView: UIView!
+    
+    override var isSelected: Bool {
+        
+        didSet{
+            UIView.animate(withDuration: 0.30) {
+                self.indicatorView.backgroundColor = self.isSelected ? UIColor.blue : UIColor.clear
+                self.layoutIfNeeded()
+            }
+        }
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        titleLabel.textAlignment = .center
+        addSubview(titleLabel)
+        addConstraintsWithFormatString(formate: "H:|[v0]|", views: titleLabel)
+        addConstraintsWithFormatString(formate: "V:|[v0]|", views: titleLabel)
+        
+        addConstraint(NSLayoutConstraint.init(item: titleLabel, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint.init(item: titleLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        
+        setupIndicatorView()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = ""
+    }
+    
+    func setupIndicatorView() {
+        indicatorView = UIView()
+        addSubview(indicatorView)
+        
+        addConstraintsWithFormatString(formate: "H:|[v0]|", views: indicatorView)
+        addConstraintsWithFormatString(formate: "V:[v0(3)]|", views: indicatorView)
+    }
+    
+}
