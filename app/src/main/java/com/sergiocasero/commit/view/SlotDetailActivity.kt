02678ba913ddfb94db18f58b8bfe8cbcdd2c7d69
Sergio Casero <sergio.casero@worldline.com>
2019-11-05 21:19:31 +0100

    Added INTERNET permission to android app

--- app/src/main/java/com/sergiocasero/commit/view/SlotDetailActivity.kt
+++ app/src/main/java/com/sergiocasero/commit/view/SlotDetailActivity.kt
@@ -4,7 +4,6 @@ import android.view.View
 import com.sergiocasero.commit.R
 import com.sergiocasero.commit.common.model.Slot
 import com.sergiocasero.commit.di.ACTIVITY_MODULE
-import com.sergiocasero.commit.presenter.Presenter
 import com.sergiocasero.commit.presenter.SlotDetailPresenter
 import com.sergiocasero.commit.presenter.SlotDetailView
 import kotlinx.android.synthetic.main.activity_slot_detail.*
@@ -30,11 +29,11 @@ class SlotDetailActivity : RootActivity<SlotDetailView>(), SlotDetailView {
     }
 
     override fun initializeUI() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Nothing to do yet
     }
 
     override fun registerListeners() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Nothing to do yet
     }
 
     override fun showMessage(message: String) {
