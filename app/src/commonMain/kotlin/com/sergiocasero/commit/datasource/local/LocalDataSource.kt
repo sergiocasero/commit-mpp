9ae3b10ba15eecc66730eb75ae973c36d6d51dc8
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2019-11-19 20:48:35 +0100

    Added local persistence

--- app/src/commonMain/kotlin/com/sergiocasero/commit/datasource/local/LocalDataSource.kt
+++ app/src/commonMain/kotlin/com/sergiocasero/commit/datasource/local/LocalDataSource.kt
@@ -1,3 +1,15 @@
 package com.sergiocasero.commit.datasource.local
 
-interface LocalDataSource
+import com.sergiocasero.commit.common.model.Slot
+import com.sergiocasero.commit.common.result.Either
+import com.sergiocasero.commit.common.result.Error
+import com.sergiocasero.commit.common.result.Success
+
+interface LocalDataSource {
+    suspend fun saveFavSlot(slot: Slot): Either<Error, Success>
+    suspend fun getFavSlots(): Either<Error, List<Slot>>
+    suspend fun remoteFavSlot(slot: Slot): Either<Error, Success>
+    suspend fun isSlotFav(slot: Slot): Either<Error, Boolean>
+
+    suspend fun clear(): Either<Error, Success>
+}
--- app/src/commonMain/kotlin/com/sergiocasero/commit/repository/CommonClientRepository.kt
+++ app/src/commonMain/kotlin/com/sergiocasero/commit/repository/CommonClientRepository.kt
@@ -6,6 +6,7 @@ import com.sergiocasero.commit.common.model.Slot
 import com.sergiocasero.commit.common.model.Track
 import com.sergiocasero.commit.common.result.Either
 import com.sergiocasero.commit.common.result.Error
+import com.sergiocasero.commit.common.result.Success
 import com.sergiocasero.commit.datasource.local.LocalDataSource
 import com.sergiocasero.commit.datasource.remote.RemoteDataSource
 
@@ -17,4 +18,12 @@ class CommonClientRepository(
     override suspend fun getDays(): Either<Error, DaysResponse> = remote.getDays()
     override suspend fun getDayTracks(dayId: Long): Either<Error, Day> = remote.getDayTracks(dayId)
     override suspend fun getTrack(trackId: Long): Either<Error, Track> = remote.getTrack(trackId)
+
+    override suspend fun getFavSlots(): Either<Error, List<Slot>> = local.getFavSlots()
+    override suspend fun updateFavSlot(slot: Slot, save: Boolean): Either<Error, Success> = when(save) {
+        true -> local.saveFavSlot(slot)
+        false -> local.remoteFavSlot(slot)
+    }
+
+    override suspend fun isSlotFav(slot: Slot): Either<Error, Boolean> = local.isSlotFav(slot)
 }
